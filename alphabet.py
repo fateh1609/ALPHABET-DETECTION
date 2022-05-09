7fa52124509c8f329f7efb6425d09a7c79e05688
fateh <71831837+fateh1609@users.noreply.github.com>
2022-05-09 18:22:16 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -5,18 +5,12 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from PIL import Image
 import PIL.ImageOps
-import os,ssl,time
-
-#setting up https context to fetch data
-if (not os.environ.get('PYTHONHTTPSVERIFY','')and
-   getattr(ssl,'_create_unverified_context',None)):
-   ssl._create_default_https_context = ssl._create_unverified_context
+import os,time
 
 X = np.load('image.npz')['arr_0']
 y = pd.read_csv("labels.csv")["labels"]
@@ -47,30 +41,24 @@ while(True):
         botom_right = (int(width/2+56),int(height/2+56))
         cv2.rectangle(gray,upper_left,botom_right,(0,255,0),2)
 
-        # roi = region of interest
         roi = gray[upper_left[1]:botom_right[1],upper_left[0]:botom_right[0]]
 
-        #cv2 -> pil format
         im_pil = Image.fromarray(roi)
 
-        # convert t0 grey scale image - "L" format means each pixel is reprented by a single value 0->255
-        image_vw = im_pil.convert("L")
-        image_vw_resized = image_vw.resize((28,28),Image.ANTIALIAS)
+        image_vb = im_pil.convert("L")
+        image_bw_resized = image_bw.resize((28,28),Image.ANTIALIAS)
 
-        #invert the image
-        image_vw_resized_inverted = PIL.ImageOps.invert(image_vw_resized)
+        image_bw_resized_inverted = PIL.ImageOps.invert(image_bw_resized)
         pixel_filter = 20
-        min_pixel = np.percentile(image_vw_resized_inverted,pixel_filter)
-        image_vw_resized_inverted_scaled = np.clip(image_vw_resized_inverted - min_pixel,0,255)
-        max_pixel = np.max(image_vw_resized_inverted)
+        min_pixel = np.percentile(image_bw_resized_inverted,pixel_filter)
+        image_bw_resized_inverted_scaled = np.clip(image_bw_resized_inverted - min_pixel,0,255)
+        max_pixel = np.max(image_bw_resized_inverted)
         
-        #converting to an array
-        image_vw_resized_inverted_scaled = np.asarray(image_vw_resized_inverted_scaled)/max_pixel
-        test_sample = np.array(image_vw_resized_inverted_scaled).reshape(1,784)
+        image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
+        test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,784)
         test_pred = clf.predict(test_sample)
         print("predicted classes: " , test_pred)
 
-        #display the resulting frame
         cv2.imshow("frame",gray)
         if cv2.waitKey(1)& 0xFF == ord("q"):
             break
